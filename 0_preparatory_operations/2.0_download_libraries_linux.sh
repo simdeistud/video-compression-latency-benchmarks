a659e4dd8fdcce5476f55d55e715b2f0f83855b9
simdeistud <simone.deiana@studenti.units.it>
2025-07-15 15:56:09 +0200

    Added documentation of which libraries to download. Initial bash script

---
+++ 0_preparatory_operations/2.0_download_libraries_linux.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Dowload libjpeg version 9f (14-01-2024)
+mkdir ../2_libraries/libjpeg
+wget -O ../2_libraries/libjpeg/src.zip https://ijg.org/files/jpegsr9f.zip
+unzip ../2_libraries/libjpeg/src.zip -d ../2_libraries/libjpeg
+rm ../2_libraries/libjpeg/src.zip
+cp -r ../2_libraries/libjpeg/jpeg-9f/* ../2_libraries/libjpeg/
+rm -r ../2_libraries/libjpeg/jpeg-9f
+
+# Dowload libjpeg-turbo version 3.1.1 (10-06-2025)
+mkdir ../2_libraries/libjpeg-turbo
+wget -O ../2_libraries/libjpeg-turbo/src.zip https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/3.1.1.zip
+unzip ../2_libraries/libjpeg-turbo/src.zip -d ../2_libraries/libjpeg-turbo
+rm ../2_libraries/libjpeg-turbo/src.zip
+cp -r ../2_libraries/libjpeg-turbo/libjpeg-turbo-3.1.1/* ../2_libraries/libjpeg-turbo/
+rm -r ../2_libraries/libjpeg-turbo/libjpeg-turbo-3.1.1
+
+# Dowload jpegli git version
+git -C ../2_libraries clone https://github.com/google/jpegli.git
+
+# Dowload sjpeg git version
+git -C ../2_libraries clone https://github.com/webmproject/sjpeg.git
+
+# Dowload gpujpeg version 0.27.7 (10-07-2025)
+mkdir ../2_libraries/gpujpeg
+wget -O ../2_libraries/gpujpeg/src.zip https://github.com/CESNET/GPUJPEG/archive/refs/tags/v0.25.7.zip
+unzip ../2_libraries/gpujpeg/src.zip -d ../2_libraries/gpujpeg
+rm ../2_libraries/gpujpeg/src.zip
+cp -r ../2_libraries/gpujpeg/GPUJPEG-0.25.7/* ../2_libraries/gpujpeg/
+rm -r ../2_libraries/gpujpeg/GPUJPEG-0.25.7
