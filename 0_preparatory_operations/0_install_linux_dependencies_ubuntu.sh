d3279a830ebecc4f9179099a2507ce92df670361
simdeistud <simone.deiana@studenti.units.it>
2025-10-06 13:53:19 +0200

    Added vulkan sdk

--- 0_preparatory_operations/0_install_linux_dependencies_ubuntu.sh
+++ 0_preparatory_operations/0_install_linux_dependencies_ubuntu.sh
@@ -7,10 +7,14 @@ sudo apt-get install -y cuda-toolkit nvidia-cuda-toolkit nvidia-gds
 
 rustup update beta
 
+wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-noble.list https://packages.lunarg.com/vulkan/lunarg-vulkan-noble.list
+sudo apt-get update
+sudo apt-get install -y vulkan-sdk
+
 #git clone https://github.com/intel/libvpl
 #cd libvpl
 #sudo script/bootstrap
 #cmake -B _build -DCMAKE_INSTALL_PREFIX=/usr/local/intelvpl
 #cmake --build _build
 #cmake --install _build
-
