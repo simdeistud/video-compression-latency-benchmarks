10bf7f5b8c2aa69f96b852d89a5e890886942aef
simdeistud <simone.deiana@studenti.units.it>
2025-10-17 16:46:05 +0200

    Refactored code to adhere to new API. Fixed absence of subsampling when using RGB input

--- 3_benchmarks/src/gpujpeg/gpujpeg_decode.c
+++ 3_benchmarks/src/gpujpeg/gpujpeg_decode.c
@@ -1,92 +1,154 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "libgpujpeg/gpujpeg.h"
 #include "../img_utils.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void print_syntax()
+{
+    printf("\nProgram Input Syntax:\n\n");
+    printf("  ┌──────────────────────────────────────┐\n");
+    printf("  │ -i  <iterations [1...n]>             │\n");
+    printf("  │ -b  <benchmark mode>                 │\n");
+    printf("  │ -o  <output mode [FILEPATH|-]>       │\n");
+    printf("  └──────────────────────────────────────┘\n");
+}
 
 int main(int argc, char* argv[])
-{  
-  /* Input image related data */
-  uint8_t* inbuf = NULL;
-  size_t inbuf_size = 0;
+{
+    /* Input image related data */
+    uint8_t* inbuf = NULL;
+    size_t inbuf_size = 0;
 
-  /* Output image related data */
-  uint8_t* outbuf = NULL;
-  size_t outbuf_size = 0;
+    /* Output image related data */
+    struct gpujpeg_decoder_output decoder_output;
 
-  /* Decoder data */
-  struct gpujpeg_parameters param;
-  struct gpujpeg_image_parameters param_image;
-  struct gpujpeg_decoder* decoder;
-  struct gpujpeg_decoder_output decoder_output;
-  
-  /* Benchmark data */
-  clock_t start_time, end_time;
-  double total_time = 0.0; /* Initialize total_time */
-  int iterations = 1;
-  
-  /* Input parsing */
-  
-  if (argc == 1) {
-    printf("\nProgram Input Syntax:\n\n");
-    printf("  ┌────────────────────────────────────────────────────────────┐\n");
-    printf("  │ Filepath          : JPEG imagepath                         │\n");
-    printf("  │ Iterations        : 1 ... n                                │\n");
-    printf("  └────────────────────────────────────────────────────────────┘\n");
-    return 1;
-  }
-  
-  /* Parsing iterations argv[2] */
-  if((iterations = atoi(argv[2])) < 1){
-    fprintf(stderr, "Error: No iterations\n");
-    return 1;
-  }
-  
-  if(img_load(argv[1], &inbuf, &inbuf_size)){
-    fprintf(stderr, "Error: Failed to load image from file: %s\n", argv[1]);
-    return 1;
-  }
+    /* Decoder data */
+    struct gpujpeg_parameters param;
+    struct gpujpeg_image_parameters param_image;
+    struct gpujpeg_decoder* decoder;
 
-  /* Initializing the decoder */
-  /* We use the default CUDA device, if you want to test on different ones, change the first argument of gpujpeg_init_device */
-  if (gpujpeg_init_device(0, 0)) {
-    perror("Failed to initialize GPU device");
-    return 1;
-  }
-  
-  decoder = gpujpeg_decoder_create(0);
-  if (decoder == NULL ){
-    perror("Failed to create decoder");
-    return 1;
-  }    
-  
-  //gpujpeg_decoder_set_output_format(decoder, GPUJPEG_RGB, GPUJPEG_444_U8_P012); /* Autodetect should do its thing */
-  gpujpeg_decoder_output_set_default(&decoder_output);
+    /* Benchmark data */
+    clock_t setup_start_time, setup_end_time;
+    clock_t decoding_start_time, decoding_end_time;
+    clock_t cleanup_start_time, cleanup_end_time;
 
-  /* Test run to see if everything works */
-  if (gpujpeg_decoder_decode(decoder, inbuf, inbuf_size, &decoder_output)){
-    perror("Failed to decode image");
-    return 1;
-  }
-  img_save("out.rgb", &outbuf, outbuf_size);
+    int iterations = 0;
+    char *output = NULL;
+    int benchmark = 0;
+    int opt;
 
-  start_time = clock();
-  /* Decompression begins here, parameters and input image
-  cannot be changed until it has finished */
-  for (int i = 0; i < iterations; i++) {
-    gpujpeg_decoder_decode(decoder, inbuf, inbuf_size, &decoder_output);
-  }
-  /* Decompression ends here, a new image can be loaded in
-  the input buffer and parameters can be changed
-  (if not they will remain the same) */
-  end_time = clock();
+    while ((opt = getopt(argc, argv, "i:bo:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'i': iterations = atoi(optarg);
+            break;
+        case 'b': benchmark = 1;
+            break;
+        case 'o': output = optarg;
+            break;
+        default:
+            fprintf(stderr, "Usage error\n");
+            print_syntax();
+            exit(EXIT_FAILURE);
+        }
+    }
 
-  total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-  
-  img_destroy(inbuf);
-  
-  printf("%f\n", total_time);
+    if (img_load_stdin(&inbuf, &inbuf_size))
+    {
+        fprintf(stderr, "Error: Failed to load image from stdin\n");
+        return 1;
+    }
 
-  return 0;
+    setup_start_time = clock();
+    /* Decoder setup starts here */
+    /* We use the default CUDA device, if you want to test on different ones, change the first argument of gpujpeg_init_device */
+    if (gpujpeg_init_device(0, 0))
+    {
+        perror("Failed to initialize GPU device");
+        return 1;
+    }
+
+    decoder = gpujpeg_decoder_create(0);
+    if (decoder == NULL)
+    {
+        perror("Failed to create decoder");
+        return 1;
+    }
+    gpujpeg_set_default_parameters(&param);
+    gpujpeg_image_set_default_parameters(&param_image);
+    gpujpeg_decoder_init(decoder, &param, &param_image);
+    gpujpeg_decoder_output_set_default(&decoder_output);
+    gpujpeg_decoder_set_output_format(decoder, GPUJPEG_RGB, GPUJPEG_444_U8_P012);
+    /* Encoder setup ends here */
+    setup_end_time = clock();
+
+    /* Test run to see if everything works */
+    if (gpujpeg_decoder_decode(decoder, inbuf, inbuf_size, &decoder_output))
+    {
+        perror("Failed to decode image");
+        return 1;
+    }
+
+    decoding_start_time = clock();
+    /* Decompression begins here, parameters and input image
+    cannot be changed until it has finished */
+    for (int i = 0; i < iterations; i++)
+    {
+        gpujpeg_decoder_decode(decoder, inbuf, inbuf_size, &decoder_output);
+    }
+    /* Decompression ends here, a new image can be loaded in
+    the input buffer and parameters can be changed
+    (if not they will remain the same) */
+    decoding_end_time = clock();
+
+    /* The cleanup process also deallocates the compressed image.
+     * For this reason, we perform a full temporary copy so we can
+     * correctly output it */
+    size_t outbuf_size = decoder_output.data_size;
+    uint8_t* outbuf = (uint8_t*)malloc(outbuf_size);
+    memcpy(outbuf, decoder_output.data, decoder_output.data_size);
+
+    cleanup_start_time = clock();
+    /* Encoder cleanup begins here */
+    gpujpeg_decoder_destroy(decoder);
+    /* Encoder cleanup ends here */
+    cleanup_end_time = clock();
+
+    if (benchmark)
+    {
+        double cleanup_time, encoding_time, setup_time, total_time;
+        setup_time = (double)(setup_end_time - setup_start_time) / CLOCKS_PER_SEC;
+        encoding_time = (double)(decoding_end_time - decoding_start_time) / CLOCKS_PER_SEC;
+        cleanup_time = (double)(cleanup_end_time - cleanup_start_time) / CLOCKS_PER_SEC;
+        total_time = setup_time + encoding_time + cleanup_time;
+        printf("setup:%f\ndecoding:%f\ncleanup:%f\ntotal:%f\n", setup_time, encoding_time, cleanup_time, total_time);
+    }
+
+    if (output == NULL)
+    {
+        return 0;
+    }
+
+    if (strcmp(output, "-") == 0)
+    {
+        size_t written = fwrite(outbuf, 1, outbuf_size, stdout);
+        if (written != outbuf_size)
+        {
+            perror("Couldn't write to stdout");
+            return 1;
+        }
+    }
+    else
+    {
+        if (img_save(output, &outbuf, outbuf_size))
+        {
+            return 1;
+        }
+    }
+
+    return 0;
 }
--- 3_benchmarks/src/gpujpeg/gpujpeg_encode.c
+++ 3_benchmarks/src/gpujpeg/gpujpeg_encode.c
@@ -1,155 +1,207 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "libgpujpeg/gpujpeg.h"
 #include "../img_utils.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
+void print_syntax()
+{
+    printf("\nProgram Input Syntax:\n\n");
+    printf("  ┌────────────────────────────────────────────┐\n");
+    printf("  │ -w  <width [px]>                           │\n");
+    printf("  │ -h  <height [px]>                          │\n");
+    printf("  │ -s  <subsampling [444|422|420]>            │\n");
+    printf("  │ -q  <quality [10...100]>                   │\n");
+    printf("  │ -f  <pixel format [planar|interleaved]>    │\n");
+    printf("  │ -r  <restart intervals [0...n]>            │\n");
+    printf("  │ -i  <iterations [1...n]>                   │\n");
+    printf("  │ -b  <benchmark mode>                       │\n");
+    printf("  │ -o  <output mode [FILEPATH|-]>             │\n");
+    printf("  └────────────────────────────────────────────┘\n");
+}
+
+int get_sampnum(const char* subsampling_str)
+{
+    switch (atoi(subsampling_str))
+    {
+    case 444: return GPUJPEG_SUBSAMPLING_444; // 0x11111100U
+    case 422: return GPUJPEG_SUBSAMPLING_422; // 0x21111100U
+    case 420: return GPUJPEG_SUBSAMPLING_420; // 0x22111100U
+    default: return GPUJPEG_SUBSAMPLING_UNKNOWN;
+    }
+}
+
+int is_interleaved(const char* fmt_str)
+{
+    if (strcmp(fmt_str, "planar") == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char** argv)
+{
     /* Input image related data */
-    uint8_t *inbuf = NULL;
+    uint8_t* inbuf = NULL;
     size_t inbuf_size = 0;
-    int img_h = 0;
-    int img_w = 0;
 
     /* Output image related data */
-    uint8_t *outbuf = NULL;
+    uint8_t* outbuf = NULL;
     size_t outbuf_size = 0;
 
     /* Encoder data */
     struct gpujpeg_parameters param;
     struct gpujpeg_image_parameters param_image;
-    struct gpujpeg_encoder *encoder;
+    struct gpujpeg_encoder* encoder;
     struct gpujpeg_encoder_input encoder_input;
-    unsigned int subsampling = 0;
-    int q = 0;
-    int interleaved = 0;
-    int restart_interval = 0;
 
     /* Benchmark data */
-    clock_t start_time, end_time;
-    double total_time = 0.0; /* Initialize total_time */
-    int iterations = 1;
+    clock_t setup_start_time, setup_end_time;
+    clock_t encoding_start_time, encoding_end_time;
+    clock_t cleanup_start_time, cleanup_end_time;
 
     /* Input parsing */
+    int width = 0, height = 0, quality = 0, restart_interval = 0, iterations = 0, format = 0;
+    char *subsampling_str = NULL, *fmt_str = NULL, *output = NULL;
+    int benchmark = 0;
+    int opt;
 
-    if (argc != 8) {
-        printf("\nProgram Input Syntax:\n\n");
-        printf("  ┌────────────────────────────────────────────────────────────┐\n");
-        printf("  │ Filepath          : RGB24 filepath                         │\n");
-        printf("  │ Resolution        : 3840 | 1920 | 1280  (16:9 only!)       │\n");
-        printf("  │ Iterations        : 1 ... n                                │\n");
-        printf("  │ Subsampling       : 444 | 422 | 420 | 0 (grayscale)        │\n");
-        printf("  │ Quality           : 0 ... 100                              │\n");
-        printf("  │ Interleaved       : 0 = NO | 1 = YES                       │\n");
-        printf("  │ Restart Interval  : 0 = NO | 1 = YES                       │\n");
-        printf("  └────────────────────────────────────────────────────────────┘\n");
+    while ((opt = getopt(argc, argv, "w:h:s:q:f:r:i:bo:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'w': width = atoi(optarg);
+            break;
+        case 'h': height = atoi(optarg);
+            break;
+        case 's': subsampling_str = optarg;
+            break;
+        case 'q': quality = atoi(optarg);
+            break;
+        case 'f': fmt_str = optarg;
+            break;
+        case 'r': restart_interval = atoi(optarg);
+            break;
+        case 'i': iterations = atoi(optarg);
+            break;
+        case 'b': benchmark = 1;
+            break;
+        case 'o': output = optarg;
+            break;
+        default:
+            fprintf(stderr, "Usage error\n");
+            print_syntax();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (img_load_stdin(&inbuf, &inbuf_size))
+    {
+        fprintf(stderr, "Error: Failed to load image from stdin\n");
         return 1;
     }
 
-
-    /* Parsing Resolution argv[2] */
-    if ((img_w = atoi(argv[2])) == 0) {
-        fprintf(stderr, "Error: Failed to parse image resolution: %s\n", argv[2]);
-        return 1;
-    }
-    switch (img_w) {
-        case 3840: img_h = 2160;
-            break;
-        case 1920: img_h = 1080;
-            break;
-        case 1280: img_h = 720;
-            break;
-        default: fprintf(stderr, "Error: Resolution not supported: %s\n", argv[2]);
-            return 1;
-    }
-
-    /* Parsing iterations argv[3] */
-    if ((iterations = atoi(argv[3])) < 1) {
-        fprintf(stderr, "Error: No iterations");
-        return 1;
-    }
-
-    /* Parsing subsampling argv[4] */
-    switch (atoi(argv[4])) {
-        case 444: subsampling = 0x11111100U;
-            break;
-        case 422: subsampling = 0x21111100U;
-            break;
-        case 420: subsampling = 0x22111100U;
-            break;
-        default: subsampling = 0U;
-            break;
-    }
-
-    /* Parsing quality argv[5] */
-    q = atoi(argv[5]);
-
-    /* Parsing is interleaved argv[6] */
-    interleaved = atoi(argv[6]);
-
-    /* Parsing restart interval argv[7] */
-    restart_interval = atoi(argv[7]);
-
-    if (img_load(argv[1], &inbuf, &inbuf_size)) {
-        fprintf(stderr, "Error: Failed to load image from file: %s\n", argv[1]);
-        return 1;
-    }
-
-    /* Initializing the encoder */
+    setup_start_time = clock();
+    /* Encoder setup starts here */
     /* We use the default CUDA device, if you want to test on different ones, change the first argument of gpujpeg_init_device */
-    if (gpujpeg_init_device(0, 0)) {
+    if (gpujpeg_init_device(0, 0))
+    {
         perror("Failed to initialize GPU device");
         return 1;
     }
 
     encoder = gpujpeg_encoder_create(0);
-    if (encoder == NULL) {
+    if (encoder == NULL)
+    {
         perror("Failed to create encoder");
         return 1;
     }
 
     /* Setting up the input image parameters */
     gpujpeg_image_set_default_parameters(&param_image);
-    param_image.width = img_w;
-    param_image.height = img_h;
+    param_image.width = width;
+    param_image.height = height;
     param_image.color_space = GPUJPEG_RGB;
     param_image.pixel_format = GPUJPEG_444_U8_P012;
-    gpujpeg_parameters_chroma_subsampling(&param, subsampling);
-    gpujpeg_encoder_input_set_image(&encoder_input, inbuf);
 
     /* Setting up the compression parameters */
     gpujpeg_set_default_parameters(&param);
-    param.quality = q;
-    param.interleaved = interleaved;
-    param.segment_info = interleaved;
-    param.restart_interval = restart_interval ? (img_w != 3840 ? 8 : 32) : 0;
+    param.quality = quality;
+    param.interleaved = is_interleaved(fmt_str);
+    param.segment_info = param.interleaved;
+    param.restart_interval = restart_interval;
+    gpujpeg_parameters_chroma_subsampling(&param, get_sampnum(subsampling_str));
+    param.color_space_internal = GPUJPEG_YCBCR;
+    gpujpeg_encoder_input_set_image(&encoder_input, inbuf);
+    /* Encoder setup ends here */
+    setup_end_time = clock();
 
     /* Test run to see if everything works */
-    if (gpujpeg_encoder_encode(encoder, &param, &param_image, &encoder_input, &outbuf, &outbuf_size)) {
+    if (gpujpeg_encoder_encode(encoder, &param, &param_image, &encoder_input, &outbuf, &outbuf_size))
+    {
         perror("Failed to encode image");
         return 1;
     }
-    img_save("out.jpeg", &outbuf, outbuf_size);
 
-    start_time = clock();
+    encoding_start_time = clock();
     /* Compression begins here, parameters and input image
        cannot be changed until it has finished             */
-    for (int i = 0; i < iterations; i++) {
+    for (int i = 0; i < iterations; i++)
+    {
         gpujpeg_encoder_encode(encoder, &param, &param_image, &encoder_input, &outbuf, &outbuf_size);
     }
     /* Compression ends here, a new image can be loaded in
        the input buffer and parameters can be changed
        (if not they will remain the same)                  */
-    end_time = clock();
+    encoding_end_time = clock();
 
-    total_time = (double) (end_time - start_time) / CLOCKS_PER_SEC;
+    /* The cleanup process also deallocates the compressed image.
+     * For this reason, we perform a full temporary copy so we can
+     * correctly output it */
+    uint8_t* tmp = (uint8_t*)malloc(outbuf_size);
+    memcpy(tmp, outbuf, outbuf_size);
 
+    cleanup_start_time = clock();
+    /* Encoder cleanup begins here */
     gpujpeg_encoder_destroy(encoder);
+    /* Encoder cleanup ends here */
+    cleanup_end_time = clock();
 
-    img_destroy(inbuf);
 
-    printf("%f\n", total_time);
+    if (benchmark)
+    {
+        double cleanup_time, encoding_time, setup_time, total_time;
+        setup_time = (double)(setup_end_time - setup_start_time) / CLOCKS_PER_SEC;
+        encoding_time = (double)(encoding_end_time - encoding_start_time) / CLOCKS_PER_SEC;
+        cleanup_time = (double)(cleanup_end_time - cleanup_start_time) / CLOCKS_PER_SEC;
+        total_time = setup_time + encoding_time + cleanup_time;
+        printf("setup:%f\nencoding:%f\ncleanup:%f\ntotal:%f\n", setup_time, encoding_time, cleanup_time, total_time);
+    }
+
+    if (output == NULL)
+    {
+        return 0;
+    }
+
+    if (strcmp(output, "-") == 0)
+    {
+        size_t written = fwrite(tmp, 1, outbuf_size, stdout);
+        if (written != outbuf_size)
+        {
+            perror("Couldn't write to stdout");
+            return 1;
+        }
+    }
+    else
+    {
+        if (img_save(output, &tmp, outbuf_size))
+        {
+            return 1;
+        }
+    }
 
     return 0;
 }
