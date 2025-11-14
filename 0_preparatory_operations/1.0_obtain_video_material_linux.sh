6a175ee54191e807614f5d1b33c0d4c7faf9da9b
simdeistud <simone.deiana@studenti.units.it>
2025-11-14 18:07:06 +0100

    Added new image sources

--- 0_preparatory_operations/1.0_obtain_video_material_linux.sh
+++ 0_preparatory_operations/1.0_obtain_video_material_linux.sh
@@ -12,6 +12,17 @@ urls=(
     "https://media.xiph.org/video/derf/ElFuente/Netflix_Narrator_4096x2160_60fps_10bit_420.y4m"
     "https://media.xiph.org/video/derf/ElFuente/Netflix_Crosswalk_4096x2160_60fps_10bit_420.y4m"
     "https://media.xiph.org/video/derf/ElFuente/Netflix_SquareAndTimelapse_4096x2160_60fps_10bit_420.y4m"
+    "https://media.xiph.org/video/derf/y4m/aspen_1080p.y4m"
+    "https://media.xiph.org/video/derf/y4m/controlled_burn_1080p.y4m"
+    "https://media.xiph.org/video/derf/y4m/crowd_run_2160p50.y4m"
+    "https://media.xiph.org/video/derf/y4m/FourPeople_1280x720_60.y4m"
+    "https://media.xiph.org/video/derf/y4m/old_town_cross_444_720p50.y4m"
+    "https://media.xiph.org/video/derf/y4m/riverbed_1080p25.y4m"
+    "https://media.xiph.org/video/derf/y4m/snow_mnt_1080p.y4m"
+    "https://media.xiph.org/video/derf/y4m/720p5994_stockholm_ter.y4m"
+    "https://media.xiph.org/video/derf/y4m/vidyo1_720p_60fps.y4m"
+    "https://media.xiph.org/video/derf/ElFuente/Netflix_BoxingPractice_4096x2160_60fps_10bit_420.y4m"
+    "https://media.xiph.org/video/derf/ElFuente/Netflix_FoodMarket2_4096x2160_60fps_10bit_420.y4m"
 )
 
 # Download each file with a numbered filename
